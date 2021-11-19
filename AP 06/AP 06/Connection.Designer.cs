6cf1ae7cebdc6c58812991f7194c4e42df89bc0a
Florian <castellierflorian@gmail.com>
2021-11-19 17:18:41 +0100

    <feat> Création de projet principale
    Projet de base pour que tout le monde parte du même point

---
+++ AP 06/AP 06/Connection.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace AP_06
+{
+    partial class Connection
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Connection));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btConnexion = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 90);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "gesAMM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 499);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Identifiant :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 588);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(239, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mot de passe :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(312, 496);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 44);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(340, 585);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(191, 44);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btConnexion
+            // 
+            this.btConnexion.Location = new System.Drawing.Point(191, 681);
+            this.btConnexion.Name = "btConnexion";
+            this.btConnexion.Size = new System.Drawing.Size(253, 54);
+            this.btConnexion.TabIndex = 5;
+            this.btConnexion.Text = "Connection";
+            this.btConnexion.UseVisualStyleBackColor = true;
+            this.btConnexion.Click += new System.EventHandler(this.btConnexion_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(191, 201);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 249);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Connection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(618, 832);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btConnexion);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Connection";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btConnexion;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AP 06/AP 06/Menu.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AP_06
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.familleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.medicamentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.etapeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.etapeNormeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decisionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.workflowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.familleToolStripMenuItem,
+            this.medicamentToolStripMenuItem,
+            this.etapeToolStripMenuItem,
+            this.etapeNormeeToolStripMenuItem,
+            this.decisionToolStripMenuItem,
+            this.workflowToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(618, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // familleToolStripMenuItem
+            // 
+            this.familleToolStripMenuItem.Name = "familleToolStripMenuItem";
+            this.familleToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.familleToolStripMenuItem.Text = "Famille";
+            // 
+            // medicamentToolStripMenuItem
+            // 
+            this.medicamentToolStripMenuItem.Name = "medicamentToolStripMenuItem";
+            this.medicamentToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.medicamentToolStripMenuItem.Text = "Medicament";
+            // 
+            // etapeToolStripMenuItem
+            // 
+            this.etapeToolStripMenuItem.Name = "etapeToolStripMenuItem";
+            this.etapeToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.etapeToolStripMenuItem.Text = "Etape";
+            // 
+            // etapeNormeeToolStripMenuItem
+            // 
+            this.etapeNormeeToolStripMenuItem.Name = "etapeNormeeToolStripMenuItem";
+            this.etapeNormeeToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.etapeNormeeToolStripMenuItem.Text = "EtapeNormee";
+            // 
+            // decisionToolStripMenuItem
+            // 
+            this.decisionToolStripMenuItem.Name = "decisionToolStripMenuItem";
+            this.decisionToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.decisionToolStripMenuItem.Text = "Decision";
+            // 
+            // workflowToolStripMenuItem
+            // 
+            this.workflowToolStripMenuItem.Name = "workflowToolStripMenuItem";
+            this.workflowToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.workflowToolStripMenuItem.Text = "Workflow";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(618, 832);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem familleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem medicamentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem etapeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem etapeNormeeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decisionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem workflowToolStripMenuItem;
+    }
+}
